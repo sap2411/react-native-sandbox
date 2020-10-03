65f8c1877ab8598e9ef78d48c6069c11c960f161
sap2411 <steven.parsons.us@gmail.com>
2020-10-02 19:40:07 -0700

    adds stylesheet

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import {ProgressBarAndroid, Platform, Dimensions, Alert, Button, Text, View, ActivityIndicator, ProgressViewIOS} from "react-native";
+import { Text, View, StyleSheet  } from "react-native";
 
-const {height, width } = Dimensions.get('window')
 
 export default function App() {
-  const onButtonPress = () => {
-    Alert.alert(`${new Date().toLocaleTimeString()} button press`)
-  }
+
   return (
     <View style={{ padding: 50 }}>
-      {Platform.OS === "ios" && <ProgressViewIOS progress={0.5}/>}
-      {Platform.OS === 'android' &&
-      <ProgressBarAndroid
-      styleAttr="Horizontal"
-      indeterrminat={false}
-      color={'red'} 
-      progress={0.5}
-    />}
-
-      <ActivityIndicator size="large" color="#61DBFB" />
-      <Button title="click me" onPress={onButtonPress}/>
-      <Text>OS: {Platform.OS}</Text>
-      <Text>Height: {height}</Text>
-      <Text>Width: {width}</Text>
+      <Text>red</Text>
+      <Text>green</Text>
+      <Text>blue</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  page: {
+    marginTop: 40
+  }
+})
