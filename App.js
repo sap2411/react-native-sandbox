f798e600cf40a6ad79fb20bb6d749dced2a9e2cc
sap2411 <steven.parsons.us@gmail.com>
2020-10-02 20:03:16 -0700

    adds useState

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, Text, View, StyleSheet, Dimensions  } from "react-native";
 
 import pic from "./assets/icon.png"
 import other from "./assets/splash.png"
 
 export default function App() {
-
+const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
-    <View style={styles.page}>
-      <Image style={styles.image} source={pic}/>
-      <Image style={styles.image} source={other}/>
+    <View style={[styles.container, { backgroundColor}]}>
+      <Text onPress={() => setBackgroundColor("green")} style={styles.button}>green</Text>
+      <Text onPress={() => setBackgroundColor("red")} style={styles.button}>red</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    fontSize: 30,
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    width: 90
+    },
   page: {
     flex: 1,
     flexDirection: "row",
