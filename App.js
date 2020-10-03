5447bcb2963068c03d908d5b2eda15d6df169acc
sap2411 <steven.parsons.us@gmail.com>
2020-10-02 19:25:59 -0700

    records button presses

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {Button, Text, View, ActivityIndicator, ProgressViewIOS} from "react-native";
 
 export default function App() {
+  const onButtonPress = () => {
+    console.log(`${new Date().toLocaleTimeString()} button pre`)
+  }
   return (
     <View style={{ padding: 50 }}>
       <ProgressViewIOS progress={0.5}/>
       <ActivityIndicator size="large" color="#61DBFB" />
-      <Button title="click me" />
+      <Button title="click me" onPress={onButtonPress}/>
     </View>
   )
 }
