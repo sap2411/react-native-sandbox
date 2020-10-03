391d60cd6f8cdbb832adeba477c6e2f0d813b078
sap2411 <steven.parsons.us@gmail.com>
2020-10-02 19:30:27 -0700

    displays phone data

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Button, Text, View, ActivityIndicator, ProgressViewIOS} from "react-native";
+import {Platform, Dimensions, Alert, Button, Text, View, ActivityIndicator, ProgressViewIOS} from "react-native";
+
+const {height, width } = Dimensions.get('window')
 
 export default function App() {
   const onButtonPress = () => {
-    console.log(`${new Date().toLocaleTimeString()} button pre`)
+    Alert.alert(`${new Date().toLocaleTimeString()} button press`)
   }
   return (
     <View style={{ padding: 50 }}>
       <ProgressViewIOS progress={0.5}/>
       <ActivityIndicator size="large" color="#61DBFB" />
       <Button title="click me" onPress={onButtonPress}/>
+      <Text>OS: {Platform.OS}</Text>
+      <Text>Height: {height}</Text>
+      <Text>Width: {width}</Text>
     </View>
   )
 }
